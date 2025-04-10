877c11593970c8726d7c0c78259145a1a1fcf372
hdperez30 <david.perez@finkargo.com>
2025-04-10 15:53:39 -0500

    Organizacion de main.tf

--- testing-co/main.tf
+++ testing-co/main.tf
@@ -5,77 +5,63 @@
   
 #}
 
+# checkov:skip=CKV2_AWS_5 Justificación: SG se asocia fuera de este módulo Terraform
 resource "aws_security_group" "sg_ec2_mongo_testing" {
-    arn         = "arn:aws:ec2:us-east-2:145023115425:security-group/sg-03914dc18396e9c7c"
-    description = "Security group for mongodb databases"
-    egress      = [
-        {
-            cidr_blocks      = [
-                "0.0.0.0/0",
-            ]
-            description      = null
-            from_port        = 0
-            ipv6_cidr_blocks = [
-                "::/0",
-            ]
-            prefix_list_ids  = []
-            protocol         = "-1"
-            security_groups  = []
-            self             = false
-            to_port          = 0
-        },
-    ]
-    id          = "sg-03914dc18396e9c7c"
-    ingress     = [
-        {
-            cidr_blocks      = [
-                "10.7.7.0/24",
-                "10.12.0.0/16",
-            ]
-            description      = null
-            from_port        = 27018
-            ipv6_cidr_blocks = []
-            prefix_list_ids  = []
-            protocol         = "tcp"
-            security_groups  = []
-            self             = false
-            to_port          = 27018
-        },
-        {
-            cidr_blocks      = [
-                "10.7.7.0/24",
-            ]
-            description      = null
-            from_port        = 27019
-            ipv6_cidr_blocks = []
-            prefix_list_ids  = []
-            protocol         = "tcp"
-            security_groups  = []
-            self             = false
-            to_port          = 27019
-        },
-        {
-            cidr_blocks      = [
-                "10.7.7.0/24",
-            ]
-            description      = null
-            from_port        = 65022
-            ipv6_cidr_blocks = []
-            prefix_list_ids  = []
-            protocol         = "tcp"
-            security_groups  = []
-            self             = false
-            to_port          = 65022
-        },
-    ]
-    name        = "SG Mongo Testing"
-    name_prefix = null
-    owner_id    = "145023115425"
-    tags        = {
-        "Name" = "SG Mongo Testing"
+  name        = "SG Mongo Testing"
+  description = "Security group for mongodb databases"
+  vpc_id      = "vpc-019c03212464e3366"
+
+  ingress = [
+    {
+      description      = null
+      from_port        = 27018
+      to_port          = 27018
+      protocol         = "tcp"
+      cidr_blocks      = ["10.7.7.0/24", "10.12.0.0/16"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    },
+    {
+      description      = null
+      from_port        = 27019
+      to_port          = 27019
+      protocol         = "tcp"
+      cidr_blocks      = ["10.7.7.0/24"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    },
+    {
+      description      = null
+      from_port        = 65022
+      to_port          = 65022
+      protocol         = "tcp"
+      cidr_blocks      = ["10.7.7.0/24"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
     }
-    tags_all    = {
-        "Name" = "SG Mongo Testing"
+  ]
+
+  egress = [
+    {
+      description      = null
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
     }
-    vpc_id      = "vpc-019c03212464e3366"
+  ]
+
+  tags = {
+    "Name" = "SG Mongo Testing"
+  }
 }
