232f0e0707ac8483ed847b9cd0e40553b02c2179
hdperez30 <david.perez@finkargo.com>
2025-04-10 17:01:09 -0500

    Organizacion de main.tf 1

--- testing-co/main.tf
+++ testing-co/main.tf
@@ -5,63 +5,45 @@
   
 #}
 
-# checkov:skip=CKV2_AWS_5 Justificación: SG se asocia fuera de este módulo Terraform
+# checkov:skip=CKV2_AWS_5 Justificación: Este SG es asociado a recursos fuera de este módulo Terraform
 resource "aws_security_group" "sg_ec2_mongo_testing" {
   name        = "SG Mongo Testing"
   description = "Security group for mongodb databases"
   vpc_id      = "vpc-019c03212464e3366"
 
-  ingress = [
-    {
-      description      = null
-      from_port        = 27018
-      to_port          = 27018
-      protocol         = "tcp"
-      cidr_blocks      = ["10.7.7.0/24", "10.12.0.0/16"]
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    },
-    {
-      description      = null
-      from_port        = 27019
-      to_port          = 27019
-      protocol         = "tcp"
-      cidr_blocks      = ["10.7.7.0/24"]
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    },
-    {
-      description      = null
-      from_port        = 65022
-      to_port          = 65022
-      protocol         = "tcp"
-      cidr_blocks      = ["10.7.7.0/24"]
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    }
-  ]
+  ingress {
+    description = "MongoDB 27018"
+    from_port   = 27018
+    to_port     = 27018
+    protocol    = "tcp"
+    cidr_blocks = ["10.7.7.0/24", "10.12.0.0/16"]
+  }
 
-  egress = [
-    {
-      description      = null
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    }
-  ]
+  ingress {
+    description = "MongoDB 27019"
+    from_port   = 27019
+    to_port     = 27019
+    protocol    = "tcp"
+    cidr_blocks = ["10.7.7.0/24"]
+  }
+
+  ingress {
+    description = "MongoDB SSH 65022"
+    from_port   = 65022
+    to_port     = 65022
+    protocol    = "tcp"
+    cidr_blocks = ["10.7.7.0/24"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   tags = {
-    "Name" = "SG Mongo Testing"
+    Name = "SG Mongo Testing"
   }
 }
